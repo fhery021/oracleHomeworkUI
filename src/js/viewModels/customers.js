26f46a4f2ec4efc235679a533715c42c3e44a913
Ferenc Bondar <fhery021@gmail.com>
2020-04-21 23:34:31 +0200

    getting services by customer id

--- src/js/viewModels/customers.js
+++ src/js/viewModels/customers.js
@@ -39,9 +39,9 @@ define([
 
     // Master list and detail list observables
     self.customerDataProvider = ko.observable(); //gets data for Activities list
-    self.itemsDataProvider = ko.observable(); //gets data for Items list
+    self.servicesDataProvider = ko.observable(); //gets data for Services list
 
-    self.itemData = ko.observable(""); //holds data for Item details
+    self.serviceData = ko.observable(""); //holds data for Service details
 
     self.pieSeriesValue = ko.observableArray([]); //holds data for pie chart
 
@@ -49,6 +49,10 @@ define([
     self.customerSelected = ko.observable(false);
     self.selectedCustomer = ko.observable();
     self.firstSelectedCustomer = ko.observable();
+
+    self.serviceSelected = ko.observable(false);
+    self.selectedService = ko.observable();
+    self.firstSelectedService = ko.observable();
 
     // REST endpoint
     var RESTurl = "http://localhost:8080/api/v1/customer/";
@@ -78,17 +82,46 @@ define([
         // Populate items list using DataProvider fetch on key attribute
         // self.itemsDataProvider(new ArrayDataProvider(itemsArray, { keyAttributes: 'id' }))
 
-        var customerKey = self.firstSelectedCustomer().data.id;
+        var customerId = self.firstSelectedCustomer().data.id;
 
-        // ... rest call here
-        console.log(customerKey);
+        // Services REST call
+        // console.log(customerId);
 
+        var serviceByCustomerIdURL =
+          "http://localhost:8080/api/v1/services/customer?customerId=" +
+          customerId;
+
+        var serviceModel = Model.Model.extend({
+          urlRoot: serviceByCustomerIdURL,
+          idAttribute: 'id'
+        });
+
+        self.myService = new serviceModel();
+        var ServiceCollection = new Model.Collection.extend({
+          url: serviceByCustomerIdURL,
+          model: self.myService,
+          comparator: 'id'
+        });
+
+        self.myServiceCol = new ServiceCollection();
+        self.servicesDataProvider(new CollectionDataProvider(self.myServiceCol));
+
+        // set list view properties
         self.customerSelected(true);
+        self.serviceSelected(false);
+
+        // clear services selection
+        self.selectedService([]);
+        self.serviceData();
+
       } else {
         self.customerSelected(false);
+        self.serviceSelected(false);
       }
     };
 
+    self.selectedServiceChanged = function (event) {
+    }
     /*
      *An observable called customerDataProvider is already bound in the View file
      *from the JSON example, so you don't need to update dashboard.html
